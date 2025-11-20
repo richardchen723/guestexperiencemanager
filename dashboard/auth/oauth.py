dd175d53331eab6a1ec0454d02fc60b8ef6f8c1a
Richard <richard.chen723@gmail.com>
2025-11-20 12:30:15 -0600

    roll back

--- dashboard/auth/oauth.py
+++ dashboard/auth/oauth.py
@@ -35,8 +35,7 @@ def create_google_blueprint(app):
     google_bp = make_google_blueprint(
         client_id=config.GOOGLE_CLIENT_ID,
         client_secret=config.GOOGLE_CLIENT_SECRET,
-        scope=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'],
-        authorized_url='/auth/google/callback'
+        scope=['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
     )
     
     # Use Flask-Dance's signal to handle user creation after OAuth completes
